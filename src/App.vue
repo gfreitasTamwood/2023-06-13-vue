530b84e98e8151a2dd05105a3afc8449d1c19933
Teacher Gustavo <gfreitas@tamwoodonline.com>
2023-06-14 15:55:06 -0700

    New API code

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,47 @@
 <template>
   <div>
-    <ContactForm/>
-    <CustomTable/>
+    <ul>
+      <li v-for="product in products" :key="product.id">
+       {{ product.id }} - {{ product.name }}
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-import CustomTable from './components/CustomTable.vue'
-import ContactForm from './components/ContactForm.vue'
 
 export default {
   name: 'App',
-  components: {
-    CustomTable,
-    ContactForm
+  data() {
+    return {
+      api: "https://testapi.jasonwatmore.com/products",
+      api_2: "http://jsonplaceholder.typicode.com/posts",
+      products: [],
+      posts: []
+    }
+  },
+  methods: {
+    async readApi() {
+      try {
+        let response = await fetch(this.api);
+        this.products = await response.json();
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    async readApi_2() {
+      try {
+        let response = await fetch(this.api_2);
+        this.posts = await response.json();
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    
+  },
+  created() {
+    this.readApi();
+    this.readApi_2();
   }
 }
 </script>
